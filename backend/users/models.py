6053dbfd89246f61f72b1b90e85619a27a61b1a3
Daniel Diego Horcajuelo <dadiego91@hotmail.com>
2023-02-12 01:56:29 +0000

    updated models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+class Address(models.Model):
+    tower = models.SmallIntegerField()
+    floor = models.SmallIntegerField()
+    door = models.CharField(max_length=5)
+
+
 class User(AbstractUser):
-    pass
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    REQUIRED_FIELDS = ["email", "address"]
