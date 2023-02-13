08d7275fed4b49762371397764c0cef4e41a9173
danieldiego <daniel.diego@gdsmodellica.com>
2023-02-13 17:27:50 +0100

    updated serializers, added tests for users app

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,8 +1,25 @@
+from typing import Dict
 from rest_framework import serializers
-from .models import User
+from .models import User, Address
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
+    address = AddressSerializer()
+
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = "__all__"
+
+    def create(self, validated_data: Dict):
+        address_data: Dict = validated_data.pop("address")
+        address_ser = AddressSerializer(data=address_data)
+        address_ser.is_valid(raise_exception=True)
+        address_obj: Address = address_ser.save()
+        user_obj: User = User.objects.create(**validated_data, address=address_obj)
+        return user_obj
--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,32 @@
+import re
 from django.test import TestCase
+from .serializers import UserSerializer
+from .models import Address, User
+from rest_framework.exceptions import ValidationError
 
-# Create your tests here.
+
+class TestUserSerializer(TestCase):
+    def test_add_user_with_all_right_params(self):
+        data = {
+            "username": "test-user",
+            "email": "test@test.com",
+            "address": {"tower": 1, "floor": 3, "door": "A"},
+            "password": "test",
+        }
+        user_ser = UserSerializer(data=data)
+        user_ser.is_valid(raise_exception=True)
+        user_ser.save()
+        self.assertTrue(User.objects.exists())
+        self.assertTrue(Address.objects.exists())
+
+    def test_add_user_with_wrong_email(self):
+        data = {
+            "username": "test-user",
+            "email": "invalid_email",
+            "address": {"tower": 1, "floor": 3, "door": "A"},
+            "password": "test",
+        }
+        user_ser = UserSerializer(data=data)
+        error = re.compile("enter a valid email", flags=re.I)
+        with self.assertRaisesRegex(expected_exception=ValidationError, expected_regex=error):
+            user_ser.is_valid(raise_exception=True)
