00450a634289e81954cf1b7b190b9dea86584991
danieldiego <daniel.diego@gdsmodellica.com>
2023-02-13 17:48:36 +0100

    updated tests

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -6,26 +6,27 @@ from rest_framework.exceptions import ValidationError
 
 
 class TestUserSerializer(TestCase):
-    def test_add_user_with_all_right_params(self):
-        data = {
+    def setUp(self) -> None:
+        self.data = {
             "username": "test-user",
             "email": "test@test.com",
             "address": {"tower": 1, "floor": 3, "door": "A"},
             "password": "test",
         }
-        user_ser = UserSerializer(data=data)
+
+    def test_add_user_with_all_right_params(self):
+        user_ser = UserSerializer(data=self.data)
         user_ser.is_valid(raise_exception=True)
         user_ser.save()
         self.assertTrue(User.objects.exists())
         self.assertTrue(Address.objects.exists())
 
+    def test_add_user_with_no_username(self):
+        pass
+
     def test_add_user_with_wrong_email(self):
-        data = {
-            "username": "test-user",
-            "email": "invalid_email",
-            "address": {"tower": 1, "floor": 3, "door": "A"},
-            "password": "test",
-        }
+        data = self.data.copy()
+        data["email"] = "invalid-email"
         user_ser = UserSerializer(data=data)
         error = re.compile("enter a valid email", flags=re.I)
         with self.assertRaisesRegex(expected_exception=ValidationError, expected_regex=error):
